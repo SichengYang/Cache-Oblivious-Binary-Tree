aaa044e97867c5a4a7bdaf38974c4c5ad81e6a14
SichengYang <sicheng@wustl.edu>
2025-04-20 13:58:40 -0500

    make everything public to let things easier

--- PackMemoryArray.h
+++ PackMemoryArray.h
@@ -3,20 +3,21 @@
 
 /// @details
 /// This is a custom pack memory array that stores data in several contiguous block of memory. To make the data structure easy to use,
-/// 
+///
 /// @tparam T
 
 template <typename T>
 class PackMemoryArray
 {
-    T *top_array; // Pointer to the top array which size is sqrt(n)
+public:
+    T *top_array;     // Pointer to the top array which size is sqrt(n)
     T **bottom_array; // Pointer to the bottom array which size is n * [sqrt(n)]
 
     int size;
     int capacity;
 
     void resize(); // Function to resize if space is limited
-public:
+
     PackMemoryArray(); // Constructor to initialize the array with a default total size of 30
     ~PackMemoryArray();
 
