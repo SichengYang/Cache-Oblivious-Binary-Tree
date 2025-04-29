835fa65570e50a16b75b4c9ca3b6fa6a3284495e
SichengYang <sicheng@wustl.edu>
2025-04-29 17:20:00 -0500

    test

---
+++ insert.cpp
@@ -0,0 +1,39 @@
+#include "BTree.h"
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+int main()
+{
+    BTree<int> tree;
+
+    std::ofstream file("insert_output.csv");
+    if (!file.is_open()) {
+        std::cerr << "Failed to open the file!" << std::endl;
+        return 1;
+    }
+
+    file << "elements,time\n";
+
+    double time_count = 0;
+    for(int i = 0; i < 250001; ++i)
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        tree.insert(rand());
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        time_count += duration;
+
+        if((i % 1000) == 0)
+        {
+            file << i << "," << time_count / 1000 << "\n";
+            cout << "Insert " << i << " elements, average time: " << time_count / 1000 << endl;
+            time_count = 0;
+        }
+    }
+
+    file.close();
+    return 0;
+}
---
+++ tree_test.cpp
@@ -0,0 +1,51 @@
+#include "BTree.h"
+#include <iostream>
+
+void insert_to_tree(BTree<int>* tree, int num)
+{
+    int index = tree->insert(num);
+    cout << "Inserted " << num << " at index: " << index << std::endl;
+    tree->print_array();
+    tree->print_tree();
+    cout << "------------------------" << endl;
+    cout << endl;
+}
+
+void remove_from_tree(BTree<int>* tree, int num)
+{
+    tree->remove(num);
+    cout << "Removed " << num << endl;
+    tree->print_array();
+    tree->print_tree();
+    cout << "------------------------" << endl;
+    cout << endl;
+}
+
+int main()
+{
+    BTree<int> tree;
+    insert_to_tree(&tree, 10);
+    insert_to_tree(&tree, 20);
+    insert_to_tree(&tree, 5);
+    insert_to_tree(&tree, 15);
+    insert_to_tree(&tree, 25);
+    insert_to_tree(&tree, 30);
+    insert_to_tree(&tree, 18);
+    insert_to_tree(&tree, 1);
+    insert_to_tree(&tree, 7);
+    insert_to_tree(&tree, 3);
+    insert_to_tree(&tree, 4);
+    insert_to_tree(&tree, -1);
+    insert_to_tree(&tree, -4);
+
+    cout << "Index of 20: " << tree.search(20) << endl;
+    cout << "Index of 7: " << tree.search(7) << endl;
+
+    remove_from_tree(&tree, -1);
+    remove_from_tree(&tree, 3);
+    remove_from_tree(&tree, 20);
+    remove_from_tree(&tree, 30);
+    remove_from_tree(&tree, -100);
+
+    return 0;
+}
