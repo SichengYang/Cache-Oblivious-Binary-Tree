7d293114d7fb53f0fe6fa25dfe65efff2cd21913
SichengYang <sicheng@wustl.edu>
2025-05-01 10:23:36 -0500

    fix coments

--- BTree.h
+++ BTree.h
@@ -11,8 +11,8 @@ using namespace std;
 /// @author Sicheng Yang and Yanqing(Lily) Fu
 /// @details
 /// This is self-balancing binary tree structure that supports efficient insertion, deletion, search, and scan (via the iterator)
-/// operations. Our group review the papers about the B-Tree and implement this data structure. Unlike the common tree on the internet,
-/// we use a custom pack memory array to store the data in a contiguous block of memory. This is done to improve cache performance and
+/// operations. Our group review the papers about the B-Tree and implement this data structure. To compare the performance, we 
+/// implement this vector version.
 /// reduce memory transfers.
 /// @tparam T
 
